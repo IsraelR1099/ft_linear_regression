c1088fc868cd08b5d6ddbe95e87a6d87625d36ff
Israel <95863801+IsraelR1099@users.noreply.github.com>
2025-02-26 09:09:18 +0000

    saving image as png

--- test.py
+++ test.py
@@ -3,3 +3,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv("data.csv")
+plot = sns.jointplot(x="km", y="price", data=df, kind="reg")
+plot.savefig("plot.png")
+print("Plot saved as: plot.png")
